dc68e6d18e434da311e21db0db9972250b55ecd8
redalgorithm <shaanancurtis@ieee.org>
2020-02-02 11:23:28 -0600

    ordering at a good place

--- PizzaBox/PizzaBox.Client/Controllers/OrderController.cs
+++ PizzaBox/PizzaBox.Client/Controllers/OrderController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PizzaBox.Storing.Interfaces;
 
 namespace PizzaBox.Client.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IPizzaBoxRepository _PBrepository; 
+        public OrderController(IPizzaBoxRepository PBrepository)
+        {
+            _PBrepository = PBrepository;
+        }
         public IActionResult Step1(int id)
         {
-            return View(id);
+            return View(_PBrepository.GetStoreById(id));
         }
     }
 }
