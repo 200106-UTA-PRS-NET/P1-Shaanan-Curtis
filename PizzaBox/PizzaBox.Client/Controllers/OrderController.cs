cc31043a81080682c5a908e732339f1c36c0fcad
redalgorithm <shaanancurtis@ieee.org>
2020-02-02 17:36:47 -0600

    Preset order complete

--- PizzaBox/PizzaBox.Client/Controllers/OrderController.cs
+++ PizzaBox/PizzaBox.Client/Controllers/OrderController.cs
@@ -4,19 +4,68 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PizzaBox.Storing.Interfaces;
+using PizzaBox.Client.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace PizzaBox.Client.Controllers
 {
     public class OrderController : Controller
     {
-        private readonly IPizzaBoxRepository _PBrepository; 
+        private readonly IPizzaBoxRepository _PBrepository;
+        private readonly OrderInfoModel order_information = new OrderInfoModel();
+
         public OrderController(IPizzaBoxRepository PBrepository)
         {
             _PBrepository = PBrepository;
         }
         public IActionResult Step1(int id)
         {
-            return View(_PBrepository.GetStoreById(id));
+            Models.Assets.ShopInfo = _PBrepository.GetStoreById(id);
+
+            var sizes = GetAllSizes();
+            var model = new OrderInfoModel();
+            model.StoreId = id;
+            model.Sizes = GetSelectListItems(sizes);
+
+            return View(model);
+        }
+
+        // return a list of available sizes
+        private IEnumerable<string> GetAllSizes()
+        {
+            return new List<string>
+            {
+                "Small",
+                "Medium",
+                "Large",
+            };
+        }
+
+        // Takes a list of strings and returns a list of SelectListItem objects.
+        // renders the DropDownList
+        private IEnumerable<SelectListItem> GetSelectListItems(IEnumerable<string> elements)
+        {
+            // Empty list for holding result
+            var selectList = new List<SelectListItem>();
+
+            // For each string in elements, create a new SelectListItem object
+            // <option value="Size Name">Size Name</option>
+            foreach (var element in elements)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = element,
+                    Text = element
+                });
+            }
+
+            return selectList;
+        }
+
+        public IActionResult Step2(OrderInfoModel model)
+        {
+            
+            return View();
         }
     }
 }
--- PizzaBox/PizzaBox.Client/Models/Assets.cs
+++ PizzaBox/PizzaBox.Client/Models/Assets.cs
@@ -8,9 +8,22 @@ namespace PizzaBox.Client.Models
 {
     public static class Assets
     {
-        public static int Storeid { get; set; }
-        public static bool Session { get; set; }
+        //PER ORDER
+        public static Store ShopInfo { get; set; }
+        public static int NumPresets { get; set; }
+        public static int NumCustoms { get; set; }
         public static string Current_user { get; set; }
+        public static string Preset_seq { get; set; }
+        public static string Custom_seq { get; set; }
+        public static int Total_pizzas { get; set; }
+        public static decimal Order_cost { get; set; }
+
+        //Session Info
+        public static bool Session { get; set; }
+
+        //Locations
         public static IEnumerable<Store> Stores { get; set; }
+
+
     }
 }
