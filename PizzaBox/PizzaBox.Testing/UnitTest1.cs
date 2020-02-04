ab19b39cc0b2e31f763fb3c1d5e78cdc3dab81ab
redalgorithm <shaanancurtis@ieee.org>
2020-02-04 12:30:03 -0600

    Testing

---
+++ PizzaBox/PizzaBox.Testing/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace PizzaBox.Testing
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange - define/declare actual values and expected values
+            //Act - perform some action
+            //Assert - compare via equals(actual, expected)
+        }
+    }
+}
